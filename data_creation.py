b646e6f6ac09f25f7b63220a58c19ecd22ddfd42
RadaevKirill <146352280+RadaevKirill@users.noreply.github.com>
2024-03-07 18:39:48 +0500

    Add data_creation file

---
+++ data_creation.py
@@ -0,0 +1,34 @@
+import os
+import random
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+def generate_data(num_days: int, procent_test: float):
+    total = []
+    for i in range(num_days):
+        day = []
+        day.append(random.randint(20, 30))  # Температура
+        day.append(random.choice(['Sunny', 'Clouds', 'Rain']))  # Погода
+        day.append(random.randint(0, 5))  # Ветер
+        day.append(random.choice(['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']))  # Направление
+        day.append(random.randint(730, 760))  # Давление
+
+        total.append(day)
+    train, test = train_test_split(pd.DataFrame(total,
+                                   columns=['Temperature', 'Sky', 'Wind', 'Direction', 'Pressure']),
+                                   test_size=0.2)
+    train.to_csv('./data/train/train.csv')
+    test.to_csv('./data/test/test.csv')
+
+
+if __name__ == '__main__':
+    procent_test = 0.2
+    num_days = 30
+
+    os.makedirs('data', exist_ok=True)
+    os.makedirs('data/train', exist_ok=True)
+    os.makedirs('data/test', exist_ok=True)
+
+    generate_data(num_days, procent_test)
